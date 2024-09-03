bd1a2f0c8c143d2b0845bb11689f09b162cc172a
devyumi <yyumiwing@naver.com>
2024-09-03 23:27:51 +0900

    refactor: CustomUserDetails 변수 Member 객체로 변경
    username, name, password, authorities 모두 Member 객체로 접근 가능

--- src/main/java/com/example/mybatis_demo/config/auth/CustomUserDetails.java
+++ src/main/java/com/example/mybatis_demo/config/auth/CustomUserDetails.java
@@ -1,58 +1,42 @@
 package com.example.mybatis_demo.config.auth;
 
+import com.example.mybatis_demo.domain.Member;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @Builder
 public class CustomUserDetails implements UserDetails {
-    private String username;
-    private String password;
-    private String name;
-    private Collection<? extends GrantedAuthority> authorities;
+    private Member member;
 
     public String getName() {
-        return name;
+        return member.getName();
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(member.getRole()));
         return authorities;
     }
 
     @Override
     public String getPassword() {
-        return password;
+        return member.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return username;
+        return member.getEmail();
     }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
--- src/main/java/com/example/mybatis_demo/config/auth/CustomUserDetailsService.java
+++ src/main/java/com/example/mybatis_demo/config/auth/CustomUserDetailsService.java
@@ -3,15 +3,10 @@ package com.example.mybatis_demo.config.auth;
 import com.example.mybatis_demo.domain.Member;
 import com.example.mybatis_demo.mapper.MemberMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -26,14 +21,8 @@ public class CustomUserDetailsService implements UserDetailsService {
     }
 
     private CustomUserDetails createUserDetails(Member member) {
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority(member.getRole()));
-
         return CustomUserDetails.builder()
-                .username(member.getEmail())
-                .password(member.getPassword())
-                .authorities(authorities)
-                .name(member.getName())
+                .member(member)
                 .build();
     }
 }
