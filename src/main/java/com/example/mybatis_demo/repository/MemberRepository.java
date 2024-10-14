c8ad440fcc261835665054ae2feb164092070257
devyumi <yyumiwing@naver.com>
2024-10-14 12:09:07 +0900

    feat: Repository 추가

---
+++ src/main/java/com/example/mybatis_demo/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.example.mybatis_demo.repository;
+
+import com.example.mybatis_demo.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
