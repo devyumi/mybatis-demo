fe947f666b85f663ccb841315e098b005867204d
devyumi <yyumiwing@naver.com>
2024-10-19 16:37:42 +0900

    feat: Querydsl 적용한 findAll, count 메서드 추가

--- src/main/java/com/example/mybatis_demo/repository/ProductCustomRepository.java
+++ src/main/java/com/example/mybatis_demo/repository/ProductCustomRepository.java
@@ -1,11 +1,13 @@
 package com.example.mybatis_demo.repository;
 
 import com.example.mybatis_demo.domain.Product;
+import com.example.mybatis_demo.dto.RequestDto;
 
 import java.util.List;
 
 public interface ProductCustomRepository {
 
-    //ResponseDto findAll(RequestDto requestDto);
-    List<Product> findAll();
+    List<Product> findAll(RequestDto requestDto);
+
+    Integer count(RequestDto requestDto);
 }
--- src/main/java/com/example/mybatis_demo/repository/ProductCustomRepositoryImpl.java
+++ src/main/java/com/example/mybatis_demo/repository/ProductCustomRepositoryImpl.java
@@ -3,6 +3,9 @@ package com.example.mybatis_demo.repository;
 import com.example.mybatis_demo.domain.Product;
 import com.example.mybatis_demo.domain.QMember;
 import com.example.mybatis_demo.domain.QProduct;
+import com.example.mybatis_demo.dto.RequestDto;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -14,20 +17,60 @@ import java.util.List;
 public class ProductCustomRepositoryImpl implements ProductCustomRepository {
 
     private final JPAQueryFactory queryFactory;
+    private final QProduct product = QProduct.product;
 
     @Override
-    public List<Product> findAll() {
-        QProduct product = QProduct.product;
+    public List<Product> findAll(RequestDto requestDto) {
 
-        return queryFactory.selectFrom(product)
-                .innerJoin(product.member, QMember.member)
-                .fetchJoin()
-                .orderBy(product.id.desc())
-                .fetch();
+        //기본
+        JPAQuery<Product> result = queryFactory.selectFrom(product)
+                .innerJoin(product.member, QMember.member);
+
+        //동적쿼리
+        processWhere(result, requestDto);
+        return result.orderBy(product.id.asc())
+                .offset(requestDto.getOffset())
+                .limit(requestDto.getSize())
+                .fetchJoin().fetch();
     }
 
-//    @Override
-//    public ResponseDto findAll(RequestDto requestDto) {
-//        return null;
-//    }
+    @Override
+    public Integer count(RequestDto requestDto) {
+
+        //기본
+        JPAQuery<Long> result = queryFactory.select(product.count())
+                .from(product)
+                .innerJoin(product.member, QMember.member);
+
+        //동적쿼리
+        processWhere(result, requestDto);
+        return result.fetchFirst().intValue();
+    }
+
+    private void processWhere(JPAQuery<?> result, RequestDto requestDto) {
+        if (requestDto.getKeyword() != null) {
+
+            switch (requestDto.getType()) {
+                case "all" -> {
+                    BooleanBuilder booleanBuilder = new BooleanBuilder();
+                    booleanBuilder.or(product.name.contains(requestDto.getKeyword()));
+                    booleanBuilder.or(product.manufacturer.contains(requestDto.getKeyword()));
+                    result.where(booleanBuilder);
+                }
+
+                case "name" -> {
+                    result.where(product.name.contains(requestDto.getKeyword()));
+                }
+
+                case "manufacturer" -> {
+                    result.where(product.manufacturer.contains(requestDto.getKeyword()));
+
+                }
+
+                case "price" -> {
+                    result.where(product.price.between(requestDto.getPriceFrom(), requestDto.getPriceTo()));
+                }
+            }
+        }
+    }
 }
